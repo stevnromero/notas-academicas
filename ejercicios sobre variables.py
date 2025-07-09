6bd307e1bed6c12278a6113b75085f7ed02f0560
steven romero <johanss2015@hotmail.com>
2025-07-09 13:03:23 -0500

    exercise today

---
+++ ejercicios sobre variables.py
@@ -0,0 +1,55 @@
+# ejercicios sobre variables
+#tipos de datos
+
+
+var_1 = "hola mundo"
+var_2 = "sandra"
+num1 = 12
+
+print(var_2)
+print(num1)
+print("var_1")
+
+#tipos de valores numericos
+
+numero_entero1 = 12
+print(numero_entero1)
+
+numero_total1 = numero_entero1 + 3
+print(numero_total1)
+
+numero_total1 += 2
+print(numero_total1)
+
+valor_aritmetico = 15
+valor_aritmetico = valor_aritmetico + 2
+print(valor_aritmetico)
+
+
+resta = 2
+resta = valor_aritmetico + resta
+print(resta)
+
+multiplicacion = 12 * 12
+print(multiplicacion)
+
+multiplicacion = numero_entero1 * valor_aritmetico
+print(multiplicacion)
+
+division = multiplicacion / resta
+print(division)
+print(type(division))
+
+
+potencia = 12**2 
+modulo = 15/2
+print(potencia)
+print(modulo)
+
+
+texto = "hola mundo"
+text = 'el agua es importante "comillas dobles" para la vida'
+text2 = "eso es 'comillas simples' todo amigos"
+print(texto)
+print(text)
+print(text2)
